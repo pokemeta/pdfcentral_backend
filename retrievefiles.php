3ead982681f79d3ae4a7b6a817b5e3d44578d964
pokemeta <pokemeta202@gmail.com>
2023-09-11 17:04:56 -0600

    comment for extra placeholder

--- retrievefiles.php
+++ retrievefiles.php
@@ -7,7 +7,8 @@ require('connection.php');
 $uid = $_POST["userid"];
 $area = $_POST["area"];
 
-$query = "SELECT * FROM files_$area WHERE iduser = $uid AND active = 1";
+//$query = "SELECT * FROM files_$area WHERE iduser = $uid AND active = 1";
+$query = "SELECT * FROM files_$area WHERE active = 1";
 $statement = $conn->prepare($query);
 $statement->execute();
 
